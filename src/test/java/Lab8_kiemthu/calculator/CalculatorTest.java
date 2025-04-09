deaef361cdc3c44ee20a36258a7bb9878eabaef6
Võ Lê Thanh Lâm <baolethanhlam2001@gmail.com>
2025-04-09 18:04:43 +0700

    9/4

--- src/test/java/Lab8_kiemthu/calculator/CalculatorTest.java
+++ src/test/java/Lab8_kiemthu/calculator/CalculatorTest.java
@@ -10,7 +10,7 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        Assert.assertEquals(calc.add(2, 3), 5);
+        Assert.assertEquals(calc.add(2, 3), 4);
     }
 
     @Test
